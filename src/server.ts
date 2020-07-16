ce573152c7d52d60d364f4eac89ae4e1d2e265fb
guilhermeulbriki <guilhermeulbriki@gmail.com>
2020-07-16 11:36:21 -0300

    eslint and jest

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Setup'});
-});
+app.get('/', (request, response) => response.json({ message: 'Setup' }));
 
 app.listen(3333);
